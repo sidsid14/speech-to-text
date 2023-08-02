8fadaaee7c578a6fd488f7122c4889a97dfe1958
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2023-08-02 11:17:33 +0530

    add recognition logic

--- src/app/speech-rec/speech-rec.component.ts
+++ src/app/speech-rec/speech-rec.component.ts
@@ -8,15 +8,56 @@ import * as speech from '@tensorflow-models/speech-commands';
   styleUrls: ['./speech-rec.component.scss'],
 })
 export class SpeechRecComponent implements OnInit {
+  prediction: { score: number; word: string; timestamp: number } = {
+    score: 0,
+    word: '',
+    timestamp: 0,
+  };
+  recognizer!: speech.SpeechCommandRecognizer;
+  words!: string[];
+
   ngOnInit(): void {
     console.log(tf.engine().backend);
-    this.loadModel();
   }
 
   async loadModel() {
-    const recognizer = await speech.create('BROWSER_FFT');
-    console.log('Model Loaded');
-    await recognizer.ensureModelLoaded();
-    console.log(recognizer.wordLabels());
+    try {
+      this.recognizer = await speech.create('BROWSER_FFT');
+      console.log('Model Loaded');
+      await this.recognizer.ensureModelLoaded();
+      this.words = this.recognizer.wordLabels();
+      console.log(this.words);
+      this.recognizer.listen(
+        async (result: any) => {
+          this.getPrediction(result);
+        },
+        { probabilityThreshold: 0.9 }
+      );
+    } catch (e) {
+      await this.terminateRecognizer();
+      throw e;
+    }
+  }
+
+  getPrediction(result: any) {
+    const scores = Array.from(result.scores).map((s, i) => ({
+      score: s,
+      word: this.words[i],
+    })) as { score: number; word: string }[];
+
+    scores.sort((s1, s2) => s2.score - s1.score);
+    const predictionWithMaxProb = scores.splice(0, 1)[0];
+
+    this.prediction.word = predictionWithMaxProb.word;
+    this.prediction = {
+      word: predictionWithMaxProb.word,
+      score: predictionWithMaxProb.score,
+      timestamp: new Date().getTime(),
+    };
+  }
+
+  async terminateRecognizer() {
+    await this.recognizer.stopListening();
+    this.prediction = { score: 0, word: '', timestamp: 0 };
   }
 }
